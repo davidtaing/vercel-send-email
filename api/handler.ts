381f2fae34b76c8906d60b14eb0c5a7aee2b73eb
davidtaing <adavidtaing@gmail.com>
2023-02-26 17:58:43 +1100

    feat: handler function to send emails via SendGrid

--- api/handler.ts
+++ api/handler.ts
@@ -1,12 +1,44 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import sgMail, { MailDataRequired } from "@sendgrid/mail";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse
-) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+type Body = {
+  replyTo: string;
+  subject: string;
+  /**
+   * Plain text content for the email body.
+   * Gets overridden by `html` if both are provided.
+   */
+  text?: string;
+  /**
+   * HTML content for the email body. Overrides `text` if both are provided.
+   */
+  html?: string;
+};
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method !== "POST")
+    return res.status(405).json({ message: "Method Not Allowed" });
+
+  const { replyTo, subject, text, html } = req.body as Body;
+
+  if (!text && !html) {
+    return res.status(400).json("No text or html provided.");
+  }
+
+  const msg: MailDataRequired = {
+    replyTo,
+    subject,
+    text: text as string,
+    html,
+    to: process.env.SEND_TO_EMAIL as string,
+    from: process.env.SEND_FROM_EMAIL as string,
+  };
+
+  try {
+    const [response] = await sgMail.send(msg);
+    return res.status(response.statusCode).json(response.body);
+  } catch (err: unknown) {
+    return res.status(500).json(err);
+  }
 }
